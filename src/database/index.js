7d87f635787cd7177748a228e89f87a7bcb6e121
gbmatheus <gabrielmatheus2503@gmail.com>
2020-12-23 23:38:41 -0300

    Corrigindo conexão com o sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -11,18 +11,8 @@ const sequelize = new Sequelize(dbConfig);
 UserModel.init(sequelize);
 PatientModel.init(sequelize);
 PatientModel.associate(sequelize.models);
-
 DoctorModel.init(sequelize);
 DoctorModel.associate(sequelize.models);
-// Testing the connection
-// try {
-//   await sequelize.authenticate();
-//   console.log('Connection has been established successfully.');
-// 
-// }catch(error) {
-//   console.error('Unable to connect to the database.');
-// 
-// }
 
 module.exports = sequelize;
 
